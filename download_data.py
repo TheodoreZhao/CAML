9157adc1edeb03d92433b510d37229748a656141
Theo <zhaotheodore@gmail.com>
2019-07-26 16:41:23 +0800

    Update download_data.py

--- download_data.py
+++ download_data.py
@@ -1,3 +1,5 @@
 from hdfs import InsecureClient
+import os
 client = InsecureClient("http://10.150.144.225:50070", user = "v-kanzha")
 client.download("/SC_recommendation/caml/data","data/", overwrite=True)
+print(os.listdir("data/"))
